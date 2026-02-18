add58c38861f75321212440b3f15dec17dc1aeab
준형 <junhyung@junhyeong-ui-Macmini.local>
2026-02-18 00:36:40 -0800

    Add basic functions and tilting logic practice

--- start.py
+++ start.py
@@ -1,10 +1,26 @@
-print("I started")
-name = "Junhyung"
-print("Hello,", name)
+def say_hello():
+    print("Hello!")
 
-angle = 10
+say_hello()
 
-if angle > 5:
-    print("Robot learning forward")
-else:
-    print("Stable")
+def say_hello(name):
+    print("Hello,",name)
+
+say_hello("Junhyung")
+
+def add(a, b):
+    result = a + b
+    return result
+
+sum_value = add(3,4)
+print("sum =", sum_value)
+
+def is_tilting(andgle_deg, threshold=5):
+    if abs(angle_deg) > threshold:
+        return True
+    else:
+        return False
+    
+print(is_tilting(3))
+print(is_tilting(10))
+print(is_tilting(-8))
